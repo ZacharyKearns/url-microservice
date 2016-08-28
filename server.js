36e4fb8143e611e000d7c40eb3adab8dfc444504
Zachary Kearns <zach_kearns89@live.com>
2016-08-28 02:09:57 -0400

    added mongo uri

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var app = express();
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+
+var url = process.env.MONGOLAB_URI;
+
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+    db.close();
+  }
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,70 +21,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/:date', function (req, res) {
-  var date;
-  if (checkIfInteger(req.params.date)) {
-    date = new Date(parseInt(req.params.date) * 1000);
-    res.send({ "unix": parseInt(req.params.date), "natural": getNaturalDate(date) });
-  } else if (Date.parse(req.params.date)) {
-    date = new Date(Date.parse(req.params.date));
-    res.send({ "unix": Date.parse(req.params.date) / 1000, "natural": getNaturalDate(date) });
-  } else {
-    res.send({ "unix": null, natural: null });
-  }
-});
-
 app.listen(app.get('port'), function () {
   console.log('App listening on port ' + app.get('port'));
 });
-
-function getNaturalDate(date) {
-  var natural = date.toDateString().split(" ");
-  switch (natural[1]) {
-    case "Jan":
-    natural[1] = "January";
-    break;
-    case "Feb":
-    natural[1] = "February";
-    break;
-    case "Mar":
-    natural[1] = "March";
-    break;
-    case "Apr":
-    natural[1] = "April";
-    break;
-    case "Jun":
-    natural[1] = "June";
-    break;
-    case "Jul":
-    natural[1] = "July";
-    break;
-    case "Aug":
-    natural[1] = "August";
-    break;
-    case "Sep":
-    natural[1] = "September";
-    break;
-    case "Oct":
-    natural[1] = "October";
-    break;
-    case "Nov":
-    natural[1] = "November";
-    break;
-    case "Dec":
-    natural[1] = "December";
-    break;
-  }
-  return natural = natural[1] + " " + natural[2] + ", " + natural[3];
-}
-
-function checkIfInteger(string) {
-  var isInteger = true;
-  string = string.split("");
-  for (var i = 0; i < string.length; i++) {
-    if (isNaN(string[i])) {
-      isInteger = false;
-    }
-  }
-  return isInteger;
-}
