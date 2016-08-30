ca33c8d7ecdc2fc5671b4d0e2b8ef3b0a6b66c37
Zachary Kearns <zach_kearns89@live.com>
2016-08-30 19:00:56 -0400

    merged http and https

--- server.js
+++ server.js
@@ -6,47 +6,39 @@ var chance = require('chance').Chance();
 var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGOLAB_URI;
 
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, function(err, db) {
   assert.equal(err, null);
   console.log("Connected correctly to server.");
   db.close();
 });
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var insertDocument = function (db, callback, domain, scheme, randomHash) {
+var insertDocument = function(db, domain, scheme, randomHash) {
    db.collection('urls').insertOne({
       "url" : {
          "old" : scheme + domain,
          "short" : "localhost:5000/" + randomHash
       }
-   }, function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted a document into the urls collection.");
-    callback();
-  });
+   });
 };
 
-function isUrlValid (domain, scheme) {
+function isUrlValid(domain, scheme) {
   var wwwDots = domain.substr(4).match(/\./g) === null ? [] : domain.substr(4).match(/\./g);
   var dots = domain.match(/\./g);
   var randomHash = chance.hash({ length: 6 });
   if (domain.substr(0, 4) === 'www.' && wwwDots.length === 1 && domain.substr(4, 1) !== '.') {
     MongoClient.connect(url, function(err, db) {
-        assert.equal(err, null);
-        insertDocument(db, function() {
-        db.close();
-      }, domain, scheme, randomHash);
+      assert.equal(err, null);
+      insertDocument(db, domain, scheme, randomHash);
     });
     return { "original_url": scheme + domain, "short_url": "localhost:5000/" + randomHash };
   } else if (domain.substr(0, 4) !== 'www.' && dots.length === 1 && domain.substr(0, 1) !== '.') {
     MongoClient.connect(url, function(err, db) {
-        assert.equal(err, null);
-        insertDocument(db, function() {
-        db.close();
-      }, domain, scheme, randomHash);
+      assert.equal(err, null);
+      insertDocument(db, domain, scheme, randomHash);
     });
     return { "original_url": scheme + domain, "short_url": "localhost:5000/" + randomHash };
   } else {
@@ -54,11 +46,11 @@ function isUrlValid (domain, scheme) {
   }
 }
 
-app.get('/https?://' + ':domain', function (req, res) {
-  var scheme = req.originalUrl(1, 4) === 'http' ? 'http://' : 'https://';
-  MongoClient.connect(url, function (err, db) {
+app.get('/https?://' + ':domain', function(req, res) {
+  var scheme = req.originalUrl.substr(1, 5) === 'https' ? 'https://' : 'http://';
+  MongoClient.connect(url, function(err, db) {
     assert.equal(err, null);
-    db.collection('urls').findOne({ "url.old": req.originalUrl(1) }, function (err, doc) {
+    db.collection('urls').findOne({ "url.old": req.originalUrl.substr(1) }, function (err, doc) {
       assert.equal(err, null);
       if (doc) {
         console.log(doc.url);
@@ -70,6 +62,6 @@ app.get('/https?://' + ':domain', function (req, res) {
   });
 });
 
-app.listen(5000, function () {
+app.listen(5000, function() {
   console.log('App listening on port ' + 5000);
 });
