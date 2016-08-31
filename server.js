acdea005b4e44268d7d6a12d36e00dbc4a6f6d50
Zachary Kearns <zach_kearns89@live.com>
2016-08-30 21:54:20 -0400

    move app to heroku

--- server.js
+++ server.js
@@ -6,37 +6,37 @@ var chance = require('chance').Chance();
 var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGOLAB_URI;
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
   assert.equal(err, null);
   console.log("Connected correctly to server.");
   db.close();
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var insertDocument = function(db, domain, scheme, randomHash) {
+var insertDocument = function (db, domain, scheme, randomHash) {
    db.collection('urls').insertOne({
       "url" : {
-         "old" : scheme + domain,
+         "original" : scheme + domain,
          "short" : "localhost:5000/" + randomHash
       }
    });
 };
 
-function isUrlValid(domain, scheme) {
+function isUrlValid (domain, scheme) {
   var wwwDots = domain.substr(4).match(/\./g) === null ? [] : domain.substr(4).match(/\./g);
   var dots = domain.match(/\./g);
   var randomHash = chance.hash({ length: 6 });
   if (domain.substr(0, 4) === 'www.' && wwwDots.length === 1 && domain.substr(4, 1) !== '.') {
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
       assert.equal(err, null);
       insertDocument(db, domain, scheme, randomHash);
     });
     return { "original_url": scheme + domain, "short_url": "localhost:5000/" + randomHash };
   } else if (domain.substr(0, 4) !== 'www.' && dots.length === 1 && domain.substr(0, 1) !== '.') {
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
       assert.equal(err, null);
       insertDocument(db, domain, scheme, randomHash);
     });
@@ -46,22 +46,26 @@ function isUrlValid(domain, scheme) {
   }
 }
 
-app.get('/https?://' + ':domain', function(req, res) {
+app.get('/https?://' + ':domain', function (req, res) {
   var scheme = req.originalUrl.substr(1, 5) === 'https' ? 'https://' : 'http://';
-  MongoClient.connect(url, function(err, db) {
+  res.send(isUrlValid(req.params.domain, scheme));
+});
+
+app.get('/:hash', function (req, res) {
+  MongoClient.connect(url, function (err, db) {
     assert.equal(err, null);
-    db.collection('urls').findOne({ "url.old": req.originalUrl.substr(1) }, function (err, doc) {
+    db.collection('urls').findOne({ "url.short": "localhost:5000/" + req.params.hash }, function (err, doc) {
       assert.equal(err, null);
       if (doc) {
-        console.log(doc.url);
+        res.redirect(doc.url.original);
       } else {
-        res.send(isUrlValid(req.params.domain, scheme));
+        res.send({ "error": "not a valid url" });
       }
       db.close();
     });
   });
 });
 
-app.listen(5000, function() {
+app.listen(5000, function () {
   console.log('App listening on port ' + 5000);
 });
